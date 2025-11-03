bea181c04a04823b1ed5f0b8de9dbba81399234d
Naiara "Kim <naiaraishiyacademico@gmail.com>
2025-11-03 20:41:10 -0300

    Controller empresas, usuarios e usuarioEmpresa

---
+++ src/controllers/usuarios.js
@@ -0,0 +1,80 @@
+const db = require('../dataBase/connection');
+
+module.exports = {
+    async listarUsuarios (request, response) {
+        try {
+            return response.status(200).json (
+                {
+                    sucesso: true,
+                    mensagem: 'Lista de usuários obtida com sucesso',
+                    dados: null
+                }
+            );
+        } catch (error) {
+            return response.status (500).json (
+                {
+                    sucesso: false,
+                    mensagem: `Erro ao listar usuários: ${error.message}`,
+                    dados: null
+                }
+            );
+        }
+    },
+    async cadastrarUsuários (request, response) {
+        try {
+            return response.status(200).json (
+                {
+                    sucesso: true,
+                    mensagem: 'Cadastro de usuário obtida com sucesso',
+                    dados: null
+                }
+            );
+        } catch (error) {
+            return response.status (500).json (
+                {
+                    sucesso: false,
+                    mensagem: `Erro ao cadastrar usuário: ${error.message}`,
+                    dados: null
+                }
+            );
+        }
+    },
+    async editarUsuarios (request, response) {
+        try {
+            return response.status(200).json (
+                {
+                    sucesso: true,
+                    mensagem: 'Atualização de usuário obtida com sucesso',
+                    dados: null
+                }
+            );
+        } catch (error) {
+            return response.status (500).json (
+                {
+                    sucesso: false,
+                    mensagem: `Erro ao atualizar usuário: ${error.message}`,
+                    dados: null
+                }
+            );
+        }
+    },
+    async apagarUsuários (request, response) {
+        try {
+            return response.status(200).json (
+                {
+                    sucesso: true,
+                    mensagem: 'Exclusão de usuário obtida com sucesso',
+                    dados: null
+                }
+            );
+        } catch (error) {
+            return response.status (500).json (
+                {
+                    sucesso: false,
+                    mensagem: `Erro ao excluir usuário: ${error.message}`,
+                    dados: null
+                }
+            );
+        }
+    },
+}
