afb5d6f7bf8e7938e463bd46d2279771ac51e295
joaoromeira-etec <joaopedrodiasromeira456@gmail.com>
2025-11-14 20:27:55 -0300

    Controllers, imports e rotas. - João Pedro
    Imports, Controllers e Rotas definidos das tabelas Tipo_Documentos, Documentos, Suporte e Regime_Empresa

---
+++ src/routes/joaoPedro.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+// Importe dos controllers
+const TipoDocumentosController = require('../controllers/tipoDocumentos');
+const DocumentosController = require('../controllers/documentos');
+const SuporteController = require('../controllers/suporte');
+const RegimeEmpresaController = require('../controllers/regimeEmpresa');
+
+// Rotas para Tipo de Documentos
+router.get('/tipoDocumentos', TipoDocumentosController.listarTipoDocumentos);
+router.post('/tipoDocumentos', TipoDocumentosController.cadastrarDocumentos);   
+router.patch('/tipoDocumentos', TipoDocumentosController.editarTipoDocumentos);
+router.delete('/tipoDocumentos', TipoDocumentosController.apagarTipoDocumentos);
+
+
+// Rotas para Documentos
+router.get('/documentos', DocumentosController.listarDocumentos);
+router.post('/documentos', DocumentosController.cadastrarDocumentos);
+router.patch('/documentos', DocumentosController.editarDocumentos);
+router.delete('/documentos', DocumentosController.apagarDocumentos);
+
+
+// Rotas para Suporte
+router.get('/suporte', SuporteController.listarSuportes);
+router.post('/suporte', SuporteController.cadastrarSuporte);
+router.patch('/suporte', SuporteController.editarSuporte);
+router.delete('/suporte', SuporteController.apagarSuporte);
+
+// Rotas para Regime da Empresa
+router.get('/regimeEmpresa', RegimeEmpresaController.listarRegimeEmpresa);
+router.post('/regimeEmpresa', RegimeEmpresaController.cadastrarRegimeEmpresa);
+router.patch('/regimeEmpresa', RegimeEmpresaController.editarRegimeEmpresa);
+router.delete('/regimeEmpresa', RegimeEmpresaController.apagarRegimeEmpresa);
+
+module.exports = router;
