09a320672eb20199b27a9fb8c7cecb9cddfbee43
Naiara "Kim <naiaraishiyacademico@gmail.com>
2025-11-18 22:50:02 -0300

    Atualização cadastros usuários

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -33,6 +33,31 @@ module.exports = {
     },
     async cadastrarUsuarios (request, response) {
         try {
+
+            const {nome, razao_social, cnpj, endereco, municipio, telefone, email, tipo} = request.body;
+
+            const sql = `
+            INSERT INTO USUARIOS (usu_id, usu_nome, usu_email, usu_cpf, usu_senha_hash, usu_telefone, usu_status, usu_alterar_senha)
+            VALUES 
+                (?, ?, ?, ?, ?, ?, ?, ?);
+                `;
+            
+//            const values = [nome, emaio, cpf, senha, telefone, alterar_senha];
+//           
+//            const [result] = await db.query(sql, values);
+//
+//            const dados = {
+//                id: result.insertId,
+//                nome,
+//                razao_social,
+//                cnpj,
+//                endereco,
+//                municipio,
+//                telefone,
+//                email,
+//                tipo
+//            };
+
             return response.status(200).json (
                 {
                     sucesso: true,
@@ -44,8 +69,8 @@ module.exports = {
             return response.status (500).json (
                 {
                     sucesso: false,
-                    mensagem: `Erro ao cadastrar usuário: ${error.message}`,
-                    dados: null
+                    mensagem: `Erro ao cadastrar usuário:`,
+                    dados: error.message
                 }
             );
         }
